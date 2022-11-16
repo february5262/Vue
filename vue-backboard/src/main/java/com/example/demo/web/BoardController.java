7dd536c009a521587eac49dd99c8f17774daeed6
yoonkyoung.cho <yoonkyoung.cho@gmail.com>
2022-11-16 18:11:04 +0900

    1116

--- vue-backboard/src/main/java/com/example/demo/web/BoardController.java
+++
@@ -1,2 +0,0 @@
-package com.example.demo.web;public class BoardController {
-}
--- vue-backboard/src/main/java/com/example/vuebackboard/VueBackBoardApplication.java
+++ vue-backboard/src/main/java/com/example/vuebackboard/VueBackBoardApplication.java
@@ -1,10 +1,8 @@
-package com.example.demo;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class VueBackBoardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
